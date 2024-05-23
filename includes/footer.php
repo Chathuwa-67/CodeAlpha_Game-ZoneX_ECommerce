6387a4f4217c161ddbb04ab977924325f0cd9f5b
Gihan Chathuranga Gunathilaka <141658614+Chathuwa-67@users.noreply.github.com>
2024-05-23 23:46:50 +0530

    Set up Dataabase

---
+++ includes/footer.php
@@ -0,0 +1,4 @@
+<footer class="footer">
+    <div class="container text-center"><span class="text-muted"><b>Game ZoneX Made by Gihan Chathuranga with ❤️ | All Rights Reserved | Contact : +94 772 682 666</b></span></div>
+</footer>
+    
